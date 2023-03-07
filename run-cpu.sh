878e2122425746386187929b32c7b26f572ec223
DreamerC <dreamerwolf.tw@gmail.com>
2023-03-07 15:15:43 +0800

    fix script formation

--- run-cpu.sh
+++ run-cpu.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 tts-server --model_name "tts_models/en/ljspeech/tacotron2-DDC_ph"
--- run-cuda.sh
+++ run-cuda.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 CUDA_VISIBLE_DEVICES="0" tts-server --model_name "tts_models/en/ljspeech/tacotron2-DDC_ph" --use_cuda True
